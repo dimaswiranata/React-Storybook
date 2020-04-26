22cf47e70872577ead2c8196e726d3e66b1b7f89
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-04-26 13:32:03 +0700

    initial commit

---
+++ storybook-static/precache-manifest.d53f1c467904a64e55514d1b90ff4b2a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07df017bec18978dce76555f013bf13c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.36c1298c081754903232.bundle.js"
+  },
+  {
+    "url": "runtime~main.36c1298c081754903232.bundle.js"
+  },
+  {
+    "url": "vendors~main.36c1298c081754903232.bundle.js"
+  },
+  {
+    "url": "vendors~main.36c1298c081754903232.bundle.js.LICENSE.txt"
+  }
+]);
